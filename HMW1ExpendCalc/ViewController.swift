8b9e889e21095a3d785e02158c87261ec7b31152
Michael Zarate <michaelzarate@desktop-0lo1vub.mobile.asu.edu>
2015-09-11 11:01:15 -0700

    Initial Commit

---
+++ HMW1ExpendCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HMW1ExpendCalc
+//
+//  Created by Michael Zarate on 9/11/15.
+//  Copyright (c) 2015 Michael Zarate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
